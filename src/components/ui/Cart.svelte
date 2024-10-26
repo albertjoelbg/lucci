7743ebe9906f2777514026023b671d2750e00c11
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2024-10-26 12:22:54 -0500

    Add marquee

---
+++ src/components/ui/Cart.svelte
@@ -0,0 +1,15 @@
+<div class="relative">
+    <!-- Botón del carrito solo con el ícono -->
+    <button class="border-t-neutral-50 btn-circle p-0">
+        <!-- Ícono del carrito -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" fill="currentColor" class="bi bi-bag"
+             viewBox="0 0 16 16">
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+        </svg>
+    </button>
+
+    <!-- Indicador de cantidad (badge) -->
+    <span
+            class="badge badge-xs bg-red-600 text-white rounded-full absolute top-3 right-3 py-2 px-1">0
+    </span>
+</div>
