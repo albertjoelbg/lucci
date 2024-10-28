d90880b880a6ac6bd8e257ed4f2efdd48eb10d7a
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2024-10-28 08:43:23 -0500

    Fix carousel

--- src/components/ui/Bento.svelte
+++ src/components/ui/Bento.svelte
@@ -1,21 +1,20 @@
 <div class="container mx-auto py-4">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
         <div class="overflow-hidden">
-            <img src="/media/content/bento/banner-casacas-mujer.png" alt="Prenda 1"
+            <img src="/media/content/bento/banner-jeans-mujer.png" alt="Oferta de jeans mujer"
                  class="w-full h-auto object-cover">
         </div>
         <div class="overflow-hidden">
-            <img src="/media/content/bento/banner-jeans-mujer.png" alt="Prenda 1"
+            <img src="/media/content/bento/banner-casacas-mujer.png" alt="Oferta de casacas mujer"
                  class="w-full h-auto object-cover">
         </div>
         <div class="overflow-hidden">
-            <img src="/media/content/bento/banner-chompas-mujer.png" alt="Prenda 1"
+            <img src="/media/content/bento/banner-chompas-mujer.png" alt="Oferta de chompas mujer"
                  class="w-full h-auto object-cover">
         </div>
         <div class="overflow-hidden">
-            <img src="/media/content/bento/banner-faldas-mujer.png" alt="Prenda 1"
+            <img src="/media/content/bento/banner-faldas-mujer.png" alt="Oferta de faldas mujer"
                  class="w-full h-auto object-cover">
         </div>
-
     </div>
 </div>
--- src/components/ui/Carousel.svelte
+++ src/components/ui/Carousel.svelte
@@ -1,27 +1,52 @@
+<script>
+    let activeIndex = 0;
+
+    function setActive(index) {
+        activeIndex = index;
+    }
+</script>
+
 <section class="z-10 carousel w-full">
     <div id="slide1" class="carousel-item relative w-full">
         <video autoplay muted loop class="max-w-full object-cover rounded-lg shadow-lg">
             <source src="/media/content/carousel/slide-1-desktop.webm" type="video/webm">
             Tu navegador no soporta el video.
         </video>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" class="btn btn-primary btn-square btn-sm">❮</a>
-            <a href="#slide2" class="btn btn-primary btn-square btn-sm">❯</a>
+        <div class="absolute left-0 right-0 top-1/2 flex -translate-y-1/2 transform justify-between px-2">
+            <a href="#slide3" on:click={() => setActive(2)}
+               class="btn btn-primary btn-square btn-sm w-6">❮</a>
+            <a href="#slide2" on:click={() => setActive(1)}
+               class="btn btn-primary btn-square btn-sm w-6">❯</a>
         </div>
     </div>
     <div id="slide2" class="carousel-item relative w-full">
-        <img src="/media/content/carousel/slide-3-desktop.png" class="w-full object-cover"/>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" class="btn btn-primary btn-square btn-sm">❮</a>
-            <a href="#slide3" class="btn btn-primary btn-square btn-sm">❯</a>
+        <img src="/media/content/carousel/slide-2-desktop.png" alt="Modelos posando con anuncio de oferta"
+             class="w-full object-cover"/>
+        <div class="absolute left-0 right-0 top-1/2 flex -translate-y-1/2 transform justify-between px-2">
+            <a href="#slide1" on:click={() => setActive(0)}
+               class="btn btn-primary btn-square btn-sm w-6">❮</a>
+            <a href="#slide3" on:click={() => setActive(2)}
+               class="btn btn-primary btn-square btn-sm w-6">❯</a>
         </div>
     </div>
     <div id="slide3" class="carousel-item relative w-full">
-        <img src="/media/content/carousel/slide-2-desktop.png" class="w-full object-cover"/>
-        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" class="btn btn-primary btn-square btn-sm">❮</a>
-            <a href="#slide1" class="btn btn-primary btn-square btn-sm">❯</a>
+        <img src="/media/content/carousel/slide-3-desktop.png" alt="modelos posando con anuncios de oferta"
+             class="w-full object-cover"/>
+        <div class="absolute left-0 right-0 top-1/2 flex -translate-y-1/2 transform justify-between px-2">
+            <a href="#slide2" on:click={() => setActive(1)}
+               class="btn btn-primary btn-square btn-sm w-6">❮</a>
+            <a href="#slide1" on:click={() => setActive(0)}
+               class="btn btn-primary btn-square btn-sm w-6">❯</a>
         </div>
     </div>
-
 </section>
+
+<div class="flex items-center justify-center space-x-2 mt-2">
+    {#each Array(3).fill(0) as _, index}
+        <a
+                class="rounded-full focus:outline-none transition-all duration-200 {activeIndex === index ? 'w-3 h-3 bg-red-600' : 'w-2 h-2 bg-gray-500'}"
+                on:click={() => setActive(index)}
+                href="#slide{index + 1}"
+        ></a>
+    {/each}
+</div>
